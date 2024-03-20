5d411f9e8c33ad6c78a6c4fb4522ebad6e0f3d91
jberrazuetasubia <j_berrazuetasubia@fanshaweonline.ca>
2024-03-20 08:24:52 -0400

    fix

--- src/index.js
+++ src/index.js
@@ -7,25 +7,6 @@ import { BrowserRouter } from 'react-router-dom';
 
 
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyA05uwlkfTQ9jDrEC6Q2FpFa8L6CDma4jw",
-  authDomain: "panitafoodjuanberrazueta.firebaseapp.com",
-  projectId: "panitafoodjuanberrazueta",
-  storageBucket: "panitafoodjuanberrazueta.appspot.com",
-  messagingSenderId: "200853891885",
-  appId: "1:200853891885:web:b097579811f371dbbf41a4"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-
 
 
 

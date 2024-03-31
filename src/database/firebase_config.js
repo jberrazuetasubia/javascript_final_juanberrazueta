2c1009fc1bb12ad98ac9fa81d1d96ec198f15306
jberrazuetasubia <j_berrazuetasubia@fanshaweonline.ca>
2024-03-30 21:45:07 -0400

    adding firebase

---
+++ src/database/firebase_config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA05uwlkfTQ9jDrEC6Q2FpFa8L6CDma4jw",
+  authDomain: "panitafoodjuanberrazueta.firebaseapp.com",
+  projectId: "panitafoodjuanberrazueta",
+  storageBucket: "panitafoodjuanberrazueta.appspot.com",
+  messagingSenderId: "200853891885",
+  appId: "1:200853891885:web:b097579811f371dbbf41a4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default firebaseConfig;
+
+
